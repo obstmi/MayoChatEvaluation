03bcfbc9c862615f8801f3e17ed6b4c735af14d8
Michael Obst <michael.obst@telekom.de>
2020-03-29 12:02:33 +0200

    14. Tutorial finished

--- app/src/main/java/de/obsti/mayo_chat_evaluation/MainActivity.java
+++ app/src/main/java/de/obsti/mayo_chat_evaluation/MainActivity.java
@@ -9,10 +9,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
+    private DatabaseReference rootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +37,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-
         currentUser = mAuth.getCurrentUser();
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
 
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -51,17 +59,43 @@ public class MainActivity extends AppCompatActivity {
 
         if(currentUser == null) {
             sendUserToLoginActivity();
+        } else {
+            verifyUserExistence();
         }
+    }
+
+    private void verifyUserExistence() {
+        String currentUserID = mAuth.getCurrentUser().getUid();
+
+        rootRef.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("name").exists()) {
+                    Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                } else {
+                    sendUserToSettingsActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void sendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
+        finish();
     }
 
     private void sendUserToSettingsActivity() {
         Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingsIntent);
+        finish();
     }
 
     @Override
--- app/src/main/java/de/obsti/mayo_chat_evaluation/SettingsActivity.java
+++ app/src/main/java/de/obsti/mayo_chat_evaluation/SettingsActivity.java
@@ -1,10 +1,23 @@
 package de.obsti.mayo_chat_evaluation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -13,13 +26,59 @@ public class SettingsActivity extends AppCompatActivity {
     private Button updateAccountSettings;
     private EditText userName, userStatus;
     private CircleImageView userProfileImage;
+    private String currentUserID;
+    private FirebaseAuth mAuth;
+    private DatabaseReference rootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
         initializeFields();
+
+        updateAccountSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateSettings();
+            }
+        });
+    }
+
+    private void updateSettings() {
+        String settingsUserName = userName.getText().toString();
+        String settingsStatus = userStatus.getText().toString();
+
+        if(TextUtils.isEmpty(settingsUserName)) {
+            Toast.makeText(this, "Please insert your username..", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(settingsStatus)) {
+            Toast.makeText(this, "Please update your status..", Toast.LENGTH_SHORT).show();
+        }
+        if(!TextUtils.isEmpty(settingsUserName) && !TextUtils.isEmpty(settingsStatus)) {
+            HashMap<String, String> profileMap = new HashMap<>();
+            profileMap.put("uid", currentUserID);
+            profileMap.put("name", settingsUserName);
+            profileMap.put("status", settingsStatus);
+
+            rootRef.child("Users").child(currentUserID).setValue(profileMap)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                sendUserToMainActivity(); //kurios, dass trotzdem noch die Toast-Message angezeigt wird
+                                Toast.makeText(SettingsActivity.this, "Success: Profile update.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                String message = task.getException().toString();
+                                Toast.makeText(SettingsActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 
     private void initializeFields() {
@@ -27,5 +86,13 @@ public class SettingsActivity extends AppCompatActivity {
         userName = (EditText)findViewById(R.id.set_user_name);
         userStatus = (EditText)findViewById(R.id.set_profile_status);
         userProfileImage = (CircleImageView)findViewById(R.id.set_profile_image);
+    }
+
+    private void sendUserToMainActivity() {
+        Intent mainIntent = new Intent(SettingsActivity.this, MainActivity.class);
+        // GoBack-Button nicht erlaubt
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
