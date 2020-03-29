c2ca7fcc439bbc0048e3a8f1d1fc1b28f313fea7
Michael Obst <michael.obst@telekom.de>
2020-03-29 21:15:45 +0200

    19. Tutorial finished

--- app/src/main/java/de/obsti/mayo_chat_evaluation/GroupChatActivity.java
+++ app/src/main/java/de/obsti/mayo_chat_evaluation/GroupChatActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GroupChatActivity extends AppCompatActivity {
 
@@ -17,10 +18,15 @@ public class GroupChatActivity extends AppCompatActivity {
     private ScrollView mScrollView;
     private TextView displayTextMessages;
 
+    private String currentGroupName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat);
+
+        currentGroupName = getIntent().getExtras().getString("groupName").toString();
+        Toast.makeText(GroupChatActivity.this, currentGroupName, Toast.LENGTH_SHORT).show();
 
         initializeFields();
     }
@@ -28,7 +34,7 @@ public class GroupChatActivity extends AppCompatActivity {
     private void initializeFields() {
         mToolbar = (Toolbar)findViewById(R.id.group_chat_bar_layout);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("groupname");
+        getSupportActionBar().setTitle(currentGroupName);
 
         sendMessageButton = (ImageButton)findViewById(R.id.send_message_button);
         userMessageInput = (EditText)findViewById(R.id.input_group_message);
--- app/src/main/java/de/obsti/mayo_chat_evaluation/GroupsFragment.java
+++ app/src/main/java/de/obsti/mayo_chat_evaluation/GroupsFragment.java
@@ -1,5 +1,6 @@
 package de.obsti.mayo_chat_evaluation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,6 +27,7 @@ import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
+ * Liest die Gruppen aus der DB, zeigt sie an und leitet beim Klick auf eine Gruppe an die GroupChatActivity weiter
  */
 public class GroupsFragment extends Fragment {
 
@@ -51,6 +54,17 @@ public class GroupsFragment extends Fragment {
         initializeFields();
 
         retrieveAndDisplayGroups();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String currentGroupName = parent.getItemAtPosition(position).toString();
+
+                Intent groupChatIntent = new Intent(getContext(), GroupChatActivity.class);
+                groupChatIntent.putExtra("groupName", currentGroupName);
+                startActivity(groupChatIntent);
+            }
+        });
 
         return groupFragmentView;
     }
