e3377ed7ec35e5b62ea2998a00849bcad6e3ad6d
Michael Obst <michael.obst@telekom.de>
2020-03-29 15:54:32 +0200

    15. Tutorial finished

--- app/src/main/java/de/obsti/mayo_chat_evaluation/LoginActivity.java
+++ app/src/main/java/de/obsti/mayo_chat_evaluation/LoginActivity.java
@@ -7,6 +7,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+//import android.view.View;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
--- app/src/main/java/de/obsti/mayo_chat_evaluation/SettingsActivity.java
+++ app/src/main/java/de/obsti/mayo_chat_evaluation/SettingsActivity.java
@@ -14,8 +14,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -41,12 +44,48 @@ public class SettingsActivity extends AppCompatActivity {
 
         initializeFields();
 
+        userName.setVisibility(View.INVISIBLE);
+
         updateAccountSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 updateSettings();
             }
         });
+
+        retrieveUserInformation();
+    }
+
+    private void retrieveUserInformation() {
+        rootRef.child("Users").child(currentUserID)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        if(dataSnapshot.exists() && dataSnapshot.hasChild("name") && dataSnapshot.hasChild("image")) {
+//
+//                        }
+                        if(dataSnapshot.exists() && dataSnapshot.hasChild("name") && dataSnapshot.hasChild("status")) {
+                            String retrieveUsername = dataSnapshot.child("name").getValue().toString();
+                            String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+
+                            userName.setText(retrieveUsername);
+                            userStatus.setText(retrieveStatus);
+
+                            if(dataSnapshot.hasChild("image")) {
+                                String retrieveProfileImage = dataSnapshot.child("image").getValue().toString();
+                            }
+
+                        } else {
+                            userName.setVisibility(View.VISIBLE);
+                            Toast.makeText(SettingsActivity.this, "Please set and update your profile!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     private void updateSettings() {
