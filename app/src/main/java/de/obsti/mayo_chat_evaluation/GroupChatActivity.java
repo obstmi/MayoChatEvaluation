e9f3af7d78530bac82e6b5cdef4846ac744900fd
Michael Obst <michael.obst@telekom.de>
2020-03-29 20:53:07 +0200

    18. Tutorial finished

---
+++ app/src/main/java/de/obsti/mayo_chat_evaluation/GroupChatActivity.java
@@ -0,0 +1,38 @@
+package de.obsti.mayo_chat_evaluation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton sendMessageButton;
+    private EditText userMessageInput;
+    private ScrollView mScrollView;
+    private TextView displayTextMessages;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+
+        initializeFields();
+    }
+
+    private void initializeFields() {
+        mToolbar = (Toolbar)findViewById(R.id.group_chat_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("groupname");
+
+        sendMessageButton = (ImageButton)findViewById(R.id.send_message_button);
+        userMessageInput = (EditText)findViewById(R.id.input_group_message);
+        displayTextMessages = (TextView)findViewById(R.id.group_chat_text_display);
+        mScrollView = (ScrollView)findViewById(R.id.my_scroll_view);
+    }
+}
