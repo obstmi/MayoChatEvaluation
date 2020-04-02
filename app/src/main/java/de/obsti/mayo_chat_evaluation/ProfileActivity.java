a41036b2387380388cdb6aeb56867c8e00482539
Michael Obst <michael.obst@telekom.de>
2020-04-02 22:48:54 +0200

    33. Tutorial finished - profile activity now working

--- app/src/main/java/de/obsti/mayo_chat_evaluation/ProfileActivity.java
+++ app/src/main/java/de/obsti/mayo_chat_evaluation/ProfileActivity.java
@@ -1,21 +1,73 @@
 package de.obsti.mayo_chat_evaluation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private String receiverUserId;
+    private CircleImageView userProfileImage;
+    private TextView userProfileName, userProfileStatus;
+    private Button sendMessageRequestButton;
+
+    private DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        userRef = FirebaseDatabase.getInstance().getReference().child("users");
+
         receiverUserId = getIntent().getExtras().get("visit_user_id").toString();
 
-        Toast.makeText(this, "User-ID: " + receiverUserId, Toast.LENGTH_SHORT).show();
+        // initialisiere Members:
+        userProfileImage = (CircleImageView)findViewById(R.id.visit_profile_image);
+        userProfileName = (TextView)findViewById(R.id.visit_user_name);
+        userProfileStatus = (TextView)findViewById(R.id.visit_profile_status);
+        sendMessageRequestButton = (Button)findViewById(R.id.send_message_request_button);
+
+        // retrieve information:
+        retrieveUserInfo();
+
+    }
+    // hole Userdaten und verbinde sie mit Aktivität
+    private void retrieveUserInfo() {
+        userRef.child(receiverUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    String userName = dataSnapshot.child("name").getValue().toString();
+                    String userStatus = dataSnapshot.child("status").getValue().toString();
+
+                    userProfileName.setText(userName);
+                    userProfileStatus.setText(userStatus);
+
+                    if(dataSnapshot.hasChild("image")) {
+                        String userImage = dataSnapshot.child("image").getValue().toString();
+                        Picasso.get().load(userImage).placeholder(R.drawable.profile_image).into(userProfileImage);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
